f6f6a0ce9183dfdb7f5ea8a677389fa433a38ef8
“Ewin” <ewinoaks@gmail.com>
2024-12-18 20:44:43 +0800

    scheme: ecomm-mob + gluestack working

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,14 @@
-//const { withNativeWind: withNativeWind } = require("nativewind/metro");
-
+//52:37
 import { View, Text, FlatList } from "react-native";
 import products from "@/assets/products.json";
 import ProductListItem from "@/components/ProductListItem";
 import { Button, ButtonText } from "@/components/ui/button";
-//import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen() {
   return (
-    <Button variant="outline">
-      <ButtonText>Press Me</ButtonText>
-    </Button>
+    <FlatList
+      data={products}
+      renderItem={({ item }) => <ProductListItem product={item} />}
+    />
   );
-  //return (
-  //<FlatList
-  //data={products}
-  //renderItem={({ item }) => <ProductListItem product={item} />}
-  ///>
-  //);
 }
--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,5 +1,47 @@
 //Product Structure
 import { Text } from "react-native";
+import { Card } from "@/components/ui/card";
+import { Image } from "@/components/ui/image";
+import { VStack } from "@/components/ui/vstack";
+import { Heading } from "@/components/ui/heading";
+import { Box } from "@/components/ui/box";
+import { Button, ButtonText } from "@/components/ui/button";
+
 export default function ProductListItem({ product }) {
-  return <Text style={{ fontSize: 100 }}>{product.name}</Text>;
+  return (
+    <Card className="p-5 rounded-lg max-w-[360px] m-3">
+      <Image
+        source={{
+          uri: "https://gluestack.github.io/public-blog-video-assets/saree.png",
+        }}
+        className="mb-6 h-[240px] w-full rounded-md aspect-[4/3]"
+        alt="image"
+      />
+      <Text className="text-sm font-normal mb-2 text-typography-700">
+        Fashion Clothing
+      </Text>
+      <VStack className="mb-6">
+        <Heading size="md" className="mb-4">
+          Cotton Kurta
+        </Heading>
+        <Text size="sm">
+          Floral embroidered notch neck thread work cotton kurta in white and
+          black.
+        </Text>
+      </VStack>
+      <Box className="flex-col sm:flex-row">
+        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
+          <ButtonText size="sm">Add to cart</ButtonText>
+        </Button>
+        <Button
+          variant="outline"
+          className="px-4 py-2 border-outline-300 sm:flex-1"
+        >
+          <ButtonText size="sm" className="text-typography-600">
+            Wishlist
+          </ButtonText>
+        </Button>
+      </Box>
+    </Card>
+  );
 }
